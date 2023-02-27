203fa87fc6f19745682bb76afed42d35c76ff4c1
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-02-27 22:18:41 +0100

    Added main

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
 
 export default {
   components: {
     AppHeader,
+    AppMain,
   },
 };
 </script>
 
 <template>
   <AppHeader />
+  <main>
+    <AppMain />
+  </main>
 </template>
 
 <style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<script></script>
+
+<template>
+  <main>
+    <div class="container">
+      <span>Content goes here</span>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: #1c1c1c;
+  padding: 3rem 0;
+}
+
+span {
+  color: white;
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+</style>
