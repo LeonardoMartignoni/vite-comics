b1a80b3bfb0e28ed580ad87381417db2dc842a9f
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-02-27 22:51:36 +0100

    Fixed footer bug

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -42,60 +42,86 @@
           </li>
         </ul>
       </div>
+    </div>
 
-      <div class="middle_footer">
-        <div class="container">
-          <div class="links">
+    <div class="middle_footer">
+      <div class="container">
+        <div class="links">
+          <div class="link">
+            <h3>DC Comics</h3>
+            <ul>
+              <li>Characters</li>
+              <li>Comics</li>
+              <li>Movies</li>
+              <li>TV</li>
+              <li>Games</li>
+              <li>Videos</li>
+              <li>News</li>
+            </ul>
             <div class="link">
-              <h3>DC Comics</h3>
+              <h3>Shop</h3>
               <ul>
-                <li>Characters</li>
-                <li>Comics</li>
-                <li>Movies</li>
-                <li>TV</li>
-                <li>Games</li>
-                <li>Videos</li>
-                <li>News</li>
-              </ul>
-              <div class="link">
-                <h3>Shop</h3>
-                <ul>
-                  <li>Shop DC</li>
-                  <li>Shop DC Collectibles</li>
-                </ul>
-              </div>
-            </div>
-            <div class="link">
-              <h3>DC</h3>
-              <ul>
-                <li>Terms Of Use</li>
-                <li>Privacy policy (New)</li>
-                <li>Ad Choices</li>
-                <li>Advertising</li>
-                <li>Jobs</li>
-                <li>Subscriptions</li>
-                <li>Talent Workshops</li>
-                <li>CPSC Certificates</li>
-                <li>Ratings</li>
-                <li>Shop Help</li>
-                <li>Contact Us</li>
-              </ul>
-            </div>
-            <div class="link">
-              <h3>SITES</h3>
-              <ul>
-                <li>DC</li>
-                <li>MAD Magazine</li>
-                <li>DC Kids</li>
-                <li>DC Universe</li>
-                <li>DC Power Visa</li>
+                <li>Shop DC</li>
+                <li>Shop DC Collectibles</li>
               </ul>
             </div>
           </div>
+          <div class="link">
+            <h3>DC</h3>
+            <ul>
+              <li>Terms Of Use</li>
+              <li>Privacy policy (New)</li>
+              <li>Ad Choices</li>
+              <li>Advertising</li>
+              <li>Jobs</li>
+              <li>Subscriptions</li>
+              <li>Talent Workshops</li>
+              <li>CPSC Certificates</li>
+              <li>Ratings</li>
+              <li>Shop Help</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+          <div class="link">
+            <h3>SITES</h3>
+            <ul>
+              <li>DC</li>
+              <li>MAD Magazine</li>
+              <li>DC Kids</li>
+              <li>DC Universe</li>
+              <li>DC Power Visa</li>
+            </ul>
+          </div>
         </div>
       </div>
-
-      <div class="bottom_footer"></div>
+    </div>
+    <div class="bottom_footer">
+      <button>SIGN-UP NOW!</button>
+      <div class="socials">
+        <span>FOLLOW US</span>
+        <span class="social_link">
+          <img
+            src="../assets/img/footer-facebook.png"
+            alt=""
+          />
+          <img
+            src="../assets/img/footer-twitter.png"
+            alt=""
+          />
+          <img
+            src="../assets/img/footer-youtube.png"
+            alt=""
+          />
+          <img
+            src="../assets/img/footer-pinterest.png"
+            alt=""
+          />
+          <img
+            src="../assets/img/footer-periscope.png"
+            alt=""
+          />
+        </span>
+      </div>
     </div>
   </footer>
 </template>
