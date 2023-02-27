5bf01e9ea03ce0f67050f9a92b4e12a7c96a5174
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-02-27 22:14:34 +0100

    Added header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  components: {
+    AppHeader,
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+body {
+  font-family: "Open Sans", sans-serif;
+  color: #606060;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+.container {
+  margin: 0 auto;
+  max-width: 1200px;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,54 @@
+<script></script>
+
+<template>
+  <header>
+    <div class="container">
+      <a href="#">
+        <img
+          src="../assets/img/dc-logo.png"
+          alt="DC Logo"
+          id="logo"
+        />
+      </a>
+
+      <nav>
+        <ul>
+          <li><a href="#">Characters</a></li>
+          <li><a href="#">Comics</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">Games</a></li>
+          <li><a href="#">Collectibles</a></li>
+          <li><a href="#">Videos</a></li>
+          <li><a href="#">Fans</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Shop</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  padding: 15px 0;
+}
+
+#logo {
+  width: 70px;
+}
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+ul li {
+  display: inline-block;
+  margin-left: 2rem;
+  text-transform: uppercase;
+  font-weight: 600;
+  height: 100%;
+}
+</style>
